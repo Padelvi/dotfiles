453aff7328dbb815458416df2ed7e914567a0327
Padelvi <pablodelvillarochoa@gmail.com>
2023-10-31 20:32:58 +0100

    Swaync

--- sway/import-gsettings.sh
+++ sway/import-gsettings.sh
@@ -8,8 +8,8 @@ if [ ! -f "$config" ]; then exit 1; fi
 gnome_schema="org.gnome.desktop.interface"
 
 # Set GTK global theme
-# gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
-# gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
+gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
 
 # Set GTK icon theme
 icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
@@ -20,5 +20,5 @@ cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
 
 # Set GTK main font
-# font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
-# gsettings set "$gnome_schema" font-name "$font_name"
+font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+gsettings set "$gnome_schema" font-name "$font_name"
