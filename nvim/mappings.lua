e20aef3b96e43f95cea5503976b7a267795fc0f1
Padelvi <pablodelvillarochoa@gmail.com>
2023-11-18 20:17:01 +0100

    New mappings for compiler.nvim

--- nvim/mappings.lua
+++ nvim/mappings.lua
@@ -4,6 +4,9 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     ["gg"] = { "gg0", desc = "Go to start in normal" },
     ["G"] = { "G$", desc = "Go to end in normal" },
+    ["<F6>"] = { "<cmd>CompilerOpen<cr>", desc = "Open compiler.nvim", noremap = true, silent = true },
+    ["<S-F6>"] = { "<cmd>CompilerRedo<cr>", noremap = true },
+    ["<S-F7>"] = { "<cmd>CompilerToggleResults<cr>", noremap = true },
   },
   v = {
     ["gg"] = { "gg0", desc = "Go to start in visual" },
